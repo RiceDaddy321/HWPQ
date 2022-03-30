c22143d4606daebbc739681ea81e348c0cf772e1
RiceDaddy321 <0carlitos2@gmail.com>
2022-03-29 21:53:47 -0500

    push, front, pop, and helper methods implemented

--- minpriorityqueue.h
+++ minpriorityqueue.h
@@ -49,7 +49,14 @@ class MinPriorityQueue
 		// Must run in O(1) time.
 		T front()
 		{
-			// TODO	
+			if(size() == 0)
+			{
+				//do something
+			}
+			else
+			{
+				return H[0];
+			}
 		}
 
 		// Removes the value at the front of the MinPriorityQueue.
@@ -58,7 +65,24 @@ class MinPriorityQueue
 		// Must run in O(log(n)) time. 
 		void pop()
 		{
-			// TODO	
+			//check if empty 
+			if(size() == 0)
+			{
+				//do something
+			}
+			else
+			{
+				//return top item and delete rightmost item on bottom layer
+				//first swap
+				swap(H[0], H[H.size()-1]);
+
+				//second, delete last
+				H.pop();
+
+				//check for violations
+				int bubbleDown = 0;
+
+			}
 		}
 
 		// If x is in the MinPriorityQueue 
@@ -72,11 +96,59 @@ class MinPriorityQueue
 			// TODO
 		}
 
+		
+
 	private:
 		// You don't need any other instance variables,
 		// but you can add some if you want to.
 		vector< pair<T, int> > H; // The heap.
 		unordered_map<T, int> I;  // Maps values to their indices in H.
+
+		int parent(int i)
+		{
+			return (i-1)/2;
+		}
+
+		int lChild(int i)
+		{
+			return i * 2 + 1;
+		}
+
+		int rChild(int i)
+		{
+			return i *2 +2;
+		}
+
+		void bubbleDown(int i)
+		{
+			int lc = lChild(i);
+			int rc = rChild(i);
+			while(lc < H.size() && rc < H.size() && H[i] > min(H[lc], H[rc]))
+			{
+				//find smallest child
+				if(H[lc] < H[rc])
+				{
+					//bubble down to left child
+					swap(H[lc], H[i]);
+					i = lc;
+				}
+				else
+				{
+					swap(H[rc], H[i]);
+					i = rc;
+				}
+			}
+		}
+
+		void bubbleUp(int i)
+		{
+			int p = parent(i);
+			while(p > 0 && H[i] < H[p])
+			{
+				swap(H[p], H[i]);
+				i = p;
+			}
+		}
 };
 
 #endif 
